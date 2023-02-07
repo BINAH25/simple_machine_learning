a19da594e92c0f266c070265939740f40b29b29d
louis <visteen192@gmail.com>
2023-02-07 11:39:43 +0000

    separating the data

--- sport.py
+++ sport.py
@@ -1,4 +1,17 @@
+# importing modules
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+
+# variables
 data = pd.read_csv('sport_data.csv')
+# replacing female and male with o and 1
+data.replace(['Female','Male'],[0,1],inplace=True)
+# droping the Sport column
+x = data.drop(columns='Sport')
+y = data.drop(columns=['Age','Height','Sex'])
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3)
+
+
+
