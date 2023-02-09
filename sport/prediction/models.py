91b9ba50e5e78775888b4e80955701ba93e283aa
louis <visteen192@gmail.com>
2023-02-09 01:59:36 +0000

    model created

--- sport/prediction/models.py
+++ sport/prediction/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+GENDER = (
+    (0,'Female'),
+    (1,'Male'),
+)
+class Data(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    age = models.PositiveIntegerField(null=True)
+    height = models.PositiveIntegerField(null=True)
+    sex = models.IntegerField(choices=GENDER)
+    prediction = models.CharField(max_length=100, blank=True)
+    date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-date']
+        
+    def __str__(self):
+        return self.name
