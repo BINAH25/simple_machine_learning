54e1bd0a64a76ec81f8433a4fc917a888bc3c719
louis <visteen192@gmail.com>
2023-02-09 02:52:28 +0000

    prediction ulrs,view and html added

--- sport/prediction/views.py
+++ sport/prediction/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import *
 # Create your views here.
 
 def index(request):
-    return render(request,'dasboard/index.html')
+    form = DataForm()
+    if request.method == 'POST':
+        if form.is_valid:
+            form.save()
+            return redirect('prediction')
+        
+    context = {'form':form}
+    return render(request,'dasboard/index.html',context)
+
+
+def prediction(request):
+    return render(request,'dasboard/prediction.html')
